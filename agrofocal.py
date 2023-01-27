865ccbb55ecb0d5c0582d87cd1053862eb91ad26
samarthbadyal <89956283+samarthbadyal@users.noreply.github.com>
2023-01-26 21:36:41 -0500

    Create agrofocal.py

---
+++ agrofocal.py
@@ -0,0 +1,31 @@
+import cv2
+
+def agrofocal(fileName):
+    vid = cv2.VideoCapture(fileName)        # reads original video
+
+    vidFPS = vid.get(cv2.CAP_PROP_FPS)                  # pulls fps
+    height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)         # pulls frame height
+    width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)           # pulls frame width
+
+    ret, frame = vid.read()             # reads each frame of the video
+    fourcc = cv2.VideoWriter_fourcc('a', 'v', 'c', '1')         # fourcc code
+    writer = cv2.VideoWriter('/Users/samarthbadyal/Downloads/NewMov.mov', apiPreference = 0, 
+    fourcc = fourcc, fps = vidFPS, frameSize=(1280,720))            # videoWriter object to write new vid
+    frames = []        
+
+    while True:
+        ret, frame = vid.read()
+        if not ret:
+            break
+        frame = cv2.resize(frame,(1280,720),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)     # resizes the frame
+        frames.append(frame)        # adds frame to the list
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):       # kills the loop and ends the video
+            break
+
+    for i in frames[::-1]:          # reverses the video
+        writer.write(i) 
+
+    writer.release()
+    vid.release()
+    cv2.destroyAllWindows()
